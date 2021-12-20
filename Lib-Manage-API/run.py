f1d7988eeee35900b55ff8826ff9edbecd42aa9d
Nguyen Cong Vinh <80946737+vn-ncvinh@users.noreply.github.com>
2021-12-20 10:53:38 +0700

    Update run.py

--- Lib-Manage-API/run.py
+++ Lib-Manage-API/run.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config['JSON_AS_ASCII'] = False
 app.config['JSON_SORT_KEYS'] = False
-from OpenSSL import SSL
 
 
 from route.admin_route import *
